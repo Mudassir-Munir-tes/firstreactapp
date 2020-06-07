425580442d11e9db9e91e910642e8650ec023a35
MUDASSIRMunir <58942002+MUDASSIRMunir@users.noreply.github.com>
2020-06-07 14:04:48 +0500

    after craeting actions for testing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
     <div>
 
       Hello world
+      <h1><center>AFTER EDITING</center></h1>
+      <ol><li>created react app first through command line</li>
+      <li>Deployed/hosted on surge</li>
+      <li>Created actions on github for CI/continuous integration</li></ol>
     </div>
   );
 }
