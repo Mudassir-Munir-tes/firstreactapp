e5422771424754e13b4615160f728907f32e0a97
MUDASSIRMunir <58942002+MUDASSIRMunir@users.noreply.github.com>
2020-06-07 14:25:49 +0500

    final update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <ol><li>created react app first through command line</li>
       <li>Deployed/hosted on surge</li>
       <li>Created actions on github for CI/continuous integration </li>
-      <li>Thank you! </li>
+      <li>Thank you</li>
       </ol>
       </div>
       </div>
